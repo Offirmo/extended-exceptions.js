9c95b9cc8d281f8dba96bbb1cee93aa60d24f0ba
yej <jutard@lemonde.fr>
2014-06-24 17:09:32 +0200

    code cleaning, no functional change
    - coherency " '
    - typos
    - dead code

--- extended_exceptions.js
+++ extended_exceptions.js
@@ -21,7 +21,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 
 define(function() {
-	"use strict";
+	'use strict';
 
 	// Note : extending js errors is not trivial at all ! cf.
 	// http://stackoverflow.com/questions/17530782/extending-javascript-error
@@ -58,11 +58,12 @@ define(function() {
 		// Import everything neded into 'this' object
 		// so it has the same prototype as an Error object.
 		// We must copy manually due to Error object specificity.
-		this.message = message; // assign directly from param. If we assign from err.message, FF sometimes fails to copy for unknown reasons
+		this.message = message; // assign directly from param.
+		                        // If we assign from err.message, FF sometimes fails to copy for unknown reasons
 		this.stack = err.stack;
 
 		// fix stuff
-		this.name = "ExtendedError";
+		this.name = 'ExtendedError';
 	}
 	ExtendedError.prototype = Object.create(error_instance.prototype); // since we can't directly access Error.prototype
 	ExtendedError.prototype.constructor = ExtendedError;
@@ -76,12 +77,9 @@ define(function() {
 		}
 
 		function CustomExtendedErrorClass() {
-			//console.log("CustomExtendedErrorClass[" + name + "] constructor with args... ");
-			//console.log(arguments);
 			// Call the parent constructor
 			ParentErrorClass.prototype.constructor.apply(this, arguments);
 			this.name = name;
-			//console.log("CustomExtendedErrorClass[" + name + "].message = " + this.message);
 		}
 		CustomExtendedErrorClass.prototype = Object.create(ParentErrorClass.prototype);
 		CustomExtendedErrorClass.prototype.constructor = CustomExtendedErrorClass;
@@ -99,44 +97,44 @@ define(function() {
 	// LogicError : represent problems in the internal logic of a program;
 	// in theory, these are preventable, and even detectable before the program runs
 	// (e.g., violations of class invariants).
-	extended_exceptions.LogicError = create_custom_error("LogicError");
+	extended_exceptions.LogicError = create_custom_error('LogicError');
 
 	// invalid_argument thr. to report invalid arguments to functions.
-	extended_exceptions.InvalidArgument = create_custom_error("InvalidArgument", extended_exceptions.LogicError);
+	extended_exceptions.InvalidArgument = create_custom_error('InvalidArgument', extended_exceptions.LogicError);
 
 	// length_error thr. when an object is constructed
 	// that would exceed its maximum permitted size
 	// (e.g., a basic_string instance).
-	extended_exceptions.LengthError = create_custom_error("LengthError", extended_exceptions.LogicError);
+	extended_exceptions.LengthError = create_custom_error('LengthError', extended_exceptions.LogicError);
 
 	// out_of_range indicate an argument whose value
 	// is not within the expected range
 	// (e.g., boundary checks in basic_string).
-	extended_exceptions.OutOfRange = create_custom_error("OutOfRange", extended_exceptions.LogicError);
+	extended_exceptions.OutOfRange = create_custom_error('OutOfRange', extended_exceptions.LogicError);
 
 	// runtime_error represent problems outside the scope
 	// of a program; they cannot be easily predicted
 	// and can generally only be caught as the program executes.
-	extended_exceptions.RuntimeError = create_custom_error("RuntimeError");
+	extended_exceptions.RuntimeError = create_custom_error('RuntimeError');
 
 
 	// a second batch with other commonly needed errors
 
 	// when we encounter code not implemented
-	extended_exceptions.NotImplemented = create_custom_error("NotImplemented", extended_exceptions.RuntimeError);
+	extended_exceptions.NotImplemented = create_custom_error('NotImplemented', extended_exceptions.RuntimeError);
 
 	// when we switch case on an enumerated var and don't recognize a value (usually a newly added one)
-	extended_exceptions.UnknownEnumValue = create_custom_error("UnknownEnumValue", extended_exceptions.RuntimeError);
+	extended_exceptions.UnknownEnumValue = create_custom_error('UnknownEnumValue', extended_exceptions.RuntimeError);
 
 	// Signals that a method has been invoked at an illegal or inappropriate time.
 	// In other words, the application is not in an appropriate state for the
 	// requested operation.
-	extended_exceptions.IllegalState = create_custom_error("IllegalState", extended_exceptions.RuntimeError);
+	extended_exceptions.IllegalState = create_custom_error('IllegalState', extended_exceptions.RuntimeError);
 
-	// Signals that an invariant (= stuff the should always be true) is not met
+	// Signals that an invariant (= stuff that should always be true) is not met
 	// This should, of course, never happen...
 	// https://en.wikipedia.org/wiki/Invariant_%28computer_science%29
-	extended_exceptions.InvariantNotMet = create_custom_error("InvariantNotMet", extended_exceptions.LogicError);
+	extended_exceptions.InvariantNotMet = create_custom_error('InvariantNotMet', extended_exceptions.LogicError);
 
 
 	return extended_exceptions;
